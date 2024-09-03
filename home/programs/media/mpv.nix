1f6108d409b54a1416ceaafc7f1758654906bcac
xunuwu <xunuwu@gmail.com>
2024-09-03 21:34:39 +0200

    add mpv video cutting script

--- home/programs/media/mpv.nix
+++ home/programs/media/mpv.nix
@@ -1,4 +1,4 @@
-{
+{pkgs, ...}: {
   programs.mpv = {
     enable = true;
     config = {
@@ -29,5 +29,8 @@
         user-agent = "Mozilla/5.0 (X11; Linux x86_64; rv:109.0) Gecko/20100101 Firefox/116.0";
       };
     };
+    scripts = with pkgs.mpvScripts; [
+      cutter
+    ];
   };
 }
