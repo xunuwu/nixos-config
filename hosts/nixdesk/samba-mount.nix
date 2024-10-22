e77d8b492dff1e641d1be3436b0722727c0126b5
xunuwu <xunuwu@gmail.com>
2024-10-22 11:48:35 +0200

    new smb-mount

--- hosts/nixdesk/samba-mount.nix
+++ hosts/nixdesk/samba-mount.nix
@@ -7,11 +7,26 @@
       type = "cifs";
       options = builtins.readFile ./smbcreds;
     }
+    {
+      description = "smb hopper vault";
+      what = "//192.168.50.97/vault"; # hopper local ip
+      where = "/server/vault";
+      type = "cifs";
+      options = "uid=xun,gid=users," + (builtins.readFile ./smbcreds);
+    }
   ];
   systemd.automounts = [
     {
       requires = ["network-online.target"];
       where = "/server/transmission";
+      wantedBy = ["multi-user.target"];
+      automountConfig = {
+        TimeoutIdleSec = "10min";
+      };
+    }
+    {
+      requires = ["network-online.target"];
+      where = "/server/vault";
       wantedBy = ["multi-user.target"];
       automountConfig = {
         TimeoutIdleSec = "10min";
