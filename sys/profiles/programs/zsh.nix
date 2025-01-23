bf6e29669ea510d34796f9518972ad8960a4842f
xunuwu <xunuwu@gmail.com>
2025-01-23 08:44:37 +0100

    fix colours in 'less'

--- sys/profiles/programs/zsh.nix
+++ sys/profiles/programs/zsh.nix
@@ -1,11 +1,17 @@
-{
+{config, ...}: {
   # enable zsh autocompletion for system packages (systemd, etc)
   environment.pathsToLink = ["/share/zsh"];
+
+  environment.variables."LESS" = config.programs.less.envVariables.LESS;
 
   programs = {
     less = {
       enable = true;
-      envVariables.LESS = "--mouse";
+      envVariables.LESS = builtins.concatStringsSep " " [
+        "--mouse"
+        "--RAW-CONTROL-CHARS"
+        "--use-color"
+      ];
     };
 
     zsh = {
