13b01036787461a925686a29e30847f22721696e
xunuwu <xunuwu@gmail.com>
2025-03-09 15:39:22 +0100

    add libgl to idea

--- home/profiles/editors/jetbrains/idea.nix
+++ home/profiles/editors/jetbrains/idea.nix
@@ -1,3 +1,13 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [jetbrains.idea-ultimate];
+  home.packages = with pkgs; [
+    (symlinkJoin {
+      name = "idea-ultimate";
+      paths = [jetbrains.idea-ultimate];
+      buildInputs = [makeWrapper];
+      postBuild = ''
+        wrapProgram $out/bin/idea-ultimate \
+          --prefix LD_LIBRARY_PATH : "${lib.makeLibraryPath [libGL]}"
+      '';
+    })
+  ];
 }
