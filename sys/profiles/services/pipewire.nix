8468285e13af74b240a4283cc6406a20e3943770
xunuwu <xunuwu@gmail.com>
2025-05-20 19:45:23 +0200

    comment out pipewire quantum & rate

--- sys/profiles/services/pipewire.nix
+++ sys/profiles/services/pipewire.nix
@@ -6,38 +6,38 @@
     jack.enable = true;
     pulse.enable = true;
 
-    extraConfig = let
-      rate = 48000;
-      min-quantum = 512;
-      max-quantum = 1024;
-    in {
-      pipewire."92-quantum"."context.properties" = {
-        "default.clock.rate" = rate;
-        "default.clock.quantum" = min-quantum;
-        "default.clock.min-quantum" = min-quantum;
-        "default.clock.max-quantum" = max-quantum;
-      };
-
-      pipewire-pulse."92-quantum" = let
-        qr = "${toString min-quantum}/${toString rate}";
-      in {
-        "context.properties" = [
-          {
-            name = "libpipewire-module-protocol-pulse";
-            args = {};
-          }
-        ];
-        "pulse.properties" = {
-          "pulse.default.req" = qr;
-          "pulse.min.req" = qr;
-          "pulse.max.req" = qr;
-          "pulse.min.quantum" = qr;
-          "pulse.max.quantum" = qr;
-        };
-        "stream.properties" = {
-          "node.latency" = qr;
-        };
-      };
-    };
+    # extraConfig = let
+    #   rate = 48000;
+    #   min-quantum = 256;
+    #   max-quantum = 1024;
+    # in {
+    #   pipewire."92-quantum"."context.properties" = {
+    #     "default.clock.rate" = rate;
+    #     "default.clock.quantum" = min-quantum;
+    #     "default.clock.min-quantum" = min-quantum;
+    #     "default.clock.max-quantum" = max-quantum;
+    #   };
+    #
+    #   pipewire-pulse."92-quantum" = let
+    #     qr = "${toString min-quantum}/${toString rate}";
+    #   in {
+    #     "context.properties" = [
+    #       {
+    #         name = "libpipewire-module-protocol-pulse";
+    #         args = {};
+    #       }
+    #     ];
+    #     "pulse.properties" = {
+    #       "pulse.default.req" = qr;
+    #       "pulse.min.req" = qr;
+    #       "pulse.max.req" = qr;
+    #       "pulse.min.quantum" = qr;
+    #       "pulse.max.quantum" = qr;
+    #     };
+    #     "stream.properties" = {
+    #       "node.latency" = qr;
+    #     };
+    #   };
+    # };
   };
 }
