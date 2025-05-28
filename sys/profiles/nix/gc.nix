559f9e170b4634aff2c0dc76cab29da8d16e6e8b
xunuwu <xunuwu@gmail.com>
2025-05-28 20:14:47 +0200

    remove old gc roots before running nix gc

--- sys/profiles/nix/gc.nix
+++ sys/profiles/nix/gc.nix
@@ -1,7 +1,21 @@
 {
+  lib,
+  pkgs,
+  ...
+}: {
   nix.gc = {
     automatic = true;
     options = "--delete-older-than 14d";
   };
   nix.optimise.automatic = true;
+
+  systemd.services.remove-old-nix-gc-roots = let
+    beforeDate = "last month";
+  in {
+    description = "Remove old nix gc roots";
+    script = "exec ${lib.getExe pkgs.findutils} /nix/var/nix/gcroots/auto -not -newermt \"${beforeDate}\" -delete";
+    serviceConfig.Type = "oneshot";
+    before = ["nix-gc.service"];
+    requiredBy = ["nix-gc.service"];
+  };
 }
