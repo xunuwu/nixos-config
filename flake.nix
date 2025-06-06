b1a2c5a905f2485c88ac07c556b175f0ce3a8e67
xunuwu <xunuwu@gmail.com>
2025-06-06 10:15:09 +0200

    inline devshell in flake.nix

--- flake.nix
+++ flake.nix
@@ -23,9 +23,19 @@
 
       perSystem = {pkgs, ...}: {
         imports = [
-          ./shells
           ./pkgs
         ];
+
+        devShells.default = pkgs.mkShell {
+          name = "dots";
+          packages = with pkgs; [
+            alejandra
+            git
+            just
+            home-manager
+            sops
+          ];
+        };
 
         formatter = pkgs.alejandra;
       };
