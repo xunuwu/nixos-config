707b459bed367e1df7abbea9c70f3aec8170ce41
xunuwu <xunuwu@gmail.com>
2025-06-19 02:37:17 +0200

    rename pasted thing :p

--- home/cli/forgejo.nix
+++ home/cli/forgejo.nix
@@ -6,7 +6,7 @@
   home.packages = with pkgs; [codeberg-cli];
 
   # most of these are default except for the url
-  xdg.configFile."berg-cli/berg.toml".source = (pkgs.formats.toml {}).generate "meli-config" {
+  xdg.configFile."berg-cli/berg.toml".source = (pkgs.formats.toml {}).generate "berg-config" {
     base_url = "git.${vars.domain}";
     protocol = "https";
     no_color = false;
