c62b2d7d218dff129bf61f896af93c92d5365e16
xunuwu <xunuwu@gmail.com>
2025-06-24 00:25:08 +0200

    add patch fixing zig build --watch

--- home/develop/langs/zig.nix
+++ home/develop/langs/zig.nix
@@ -1,3 +1,8 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [zig zls];
+  home.packages = let
+    zig_patched = pkgs.zig.overrideAttrs {patches = [./__zig_watch.patch];};
+  in [
+    zig_patched
+    (pkgs.zls.override {zig_0_14 = zig_patched;})
+  ];
 }
