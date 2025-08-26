385de4ec5610c1f8124d8eb476ff90c58941ef4e
xunuwu <xunuwu@gmail.com>
2025-08-26 12:53:19 +0200

    enable exit node on default tailscale config

--- sys/profiles/network/tailscale.nix
+++ sys/profiles/network/tailscale.nix
@@ -3,6 +3,7 @@
     enable = true;
     openFirewall = true;
     useRoutingFeatures = "client";
+    extraSetFlags = ["--advertise-exit-node"];
   };
 
   environment.persistence."/persist".directories = ["/var/lib/tailscale"];
