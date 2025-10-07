7381b0eb1b9d4a3b96b6b74dcba058eec44b0a9e
xunuwu <xunuwu@gmail.com>
2025-10-07 12:00:22 +0200

    enable wsl start menu launchers on conifer

--- hosts/conifer/hardware.nix
+++ hosts/conifer/hardware.nix
@@ -2,6 +2,7 @@
   imports = [inputs.nixos-wsl.nixosModules.default];
   wsl.enable = true;
   wsl.defaultUser = "xun";
+  wsl.startMenuLaunchers = true;
 
   nixpkgs.hostPlatform.system = "x86_64-linux";
 }
